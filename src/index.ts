2cbbbaa8201031e6ceeda1c61b9a60a416ecc7f0
DutraGames <gabrieldutraamarante@gmail.com>
2021-09-11 21:15:59 -0300

    electron windows config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import {app, BrowserWindow} from 'electron'
+import {app, BrowserWindow, Menu} from 'electron'
+import {resolve} from 'path'
+
+const iconpath = resolve(__dirname, '../', 'assets', 'icon.png')
 
 function createWindow () {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+      width: 250,
+      height: 400,
+      title: "Calculdora",
+      resizable:false,
+      show: false,
+      icon: iconpath,
     })
-  
+
+    win.setMenu(null)
+
+    win.on("ready-to-show", ()=>{
+      win.show()
+    })
+
     win.loadFile('index.html')
 }
 
